0097abda61f891666491b2d52c15aaae93424acf
lemesh <lemesh2424@gmail.com>
2019-02-07 23:08:26 +0200

    Initial commit

---
+++ AddItemViewController.swift
@@ -0,0 +1,66 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Oleh Lemeshenko on 2/4/19.
+//  Copyright © 2019 Oleh Lemeshenko. All rights reserved.
+//
+
+import UIKit
+
+protocol AddItemViewControllerDelegate: class {
+  func addItemViewControllerDidCancel(_ controller: AddItemViewController)
+  func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
+}
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
+  
+  weak var delegate: AddItemViewControllerDelegate?
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+    
+    navigationItem.largeTitleDisplayMode = .never
+  }
+  
+  // MARK:- Actions
+  @IBAction func cancel() {
+    delegate?.addItemViewControllerDidCancel(self)
+  }
+  
+  @IBAction func done() {
+    let item = ChecklistItem()
+    item.text = textField.text!
+    
+    delegate?.addItemViewController(self, didFinishAdding: item)
+  }
+  
+  // MARK:- Table View Delegates
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    return nil
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
+  
+  // MARK:- Text Field Delegates
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+    let oldText = textField.text!
+    let stringRange = Range(range, in:oldText)!
+    let newText = oldText.replacingCharacters(in: stringRange, with: string)
+    
+    doneBarButton.isEnabled = !newText.isEmpty
+    
+    return true
+  }
+  
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    doneBarButton.isEnabled = false
+    return true
+  }
+}
---
+++ ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Oleh Lemeshenko on 1/31/19.
+//  Copyright © 2019 Oleh Lemeshenko. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+  
+  func toggleChecked() {
+    checked = !checked
+  }
+}
